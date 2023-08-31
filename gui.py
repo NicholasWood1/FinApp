bd46f2961319fcffdb368a77e405a8e210da60cb
N0-Lifer <mlg.wood1@gmail.com>
2023-08-31 21:30:16 +1000

    Added matplotlib plot in tkinter

--- gui.py
+++ gui.py
@@ -1,9 +1,19 @@
 from tkinter import *
 from tkinter import ttk
+
+#Creates Window
 root = Tk()
+root.title("Personal Finance")
+
 frm = ttk.Frame(root, padding=100)
 frm.grid()
+
+#Adds text to window
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
 ttk.Label(frm,text="new User??").grid(column=1, row=1)
+
+#Creates a button to exit application
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=0)
+
+#Starts the gui
 root.mainloop()
